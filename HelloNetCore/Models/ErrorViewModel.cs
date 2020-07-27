3ca291e1c4e10f0a83f092a76552bfe651bc3335
masgary <masgary@MASGARY1-PC>
2020-07-27 22:41:09 +0430

    Add project files.

---
+++ HelloNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
